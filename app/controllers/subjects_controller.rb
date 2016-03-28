d359949e2c5d7e49344540c154bee17a6ee956c6
watkins <bendesign200@gmail.com>
2016-03-28 03:43:24 +0100

    Worked on design and added edit and destroy actions

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,5 @@
 class SubjectsController < ApplicationController
-  before_action :find_subject, only: [:show]
+  before_action :find_subject, only: [:show, :edit, :update, :destroy]
 
   def index
     @subjects = Subject.order("name ASC")
@@ -8,9 +8,23 @@ class SubjectsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+    @subject.destroy
+    redirect_to dashboard_path
+  end
+
   private
 
   def find_subject
-    @subject = Subject.find(params[:id])
+    @subject = Subject.find(Base64.urlsafe_decode64(params[:id]))
   end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,5 @@
 class Subject < ActiveRecord::Base
+  def to_param
+    Base64.urlsafe_encode64 self.id.to_s
+  end
 end
