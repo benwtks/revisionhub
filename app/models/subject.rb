f8dc79e70e6961a3553026e66c0797fdf1c7998a
watkins <bendesign200@gmail.com>
2016-04-02 23:23:05 +0100

    Created migration to add subject_id to the topic model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,6 @@ class Subject < ActiveRecord::Base
   def to_param
     Base64.urlsafe_encode64 self.id.to_s
   end
+
+  has_many :topics
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402221200) do
+ActiveRecord::Schema.define(version: 20160402221812) do
 
   create_table "subjects", force: :cascade do |t|
     t.string   "teacher",    limit: 255
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160402221200) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "subject_id",  limit: 4
   end
 
 end
