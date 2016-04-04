63113e9e3d4bd6a0af35221d8f9eb00d375c4b9b
watkins <bendesign200@gmail.com>
2016-04-04 18:31:21 +0100

    Set up relationship between both the topic and session models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402221812) do
+ActiveRecord::Schema.define(version: 20160404172326) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "topic_id",   limit: 4
+    t.time     "duration"
+    t.datetime "date"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string   "teacher",    limit: 255
