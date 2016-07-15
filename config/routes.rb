c5cfcc4be2d78c5d496e68ad8a73946013904aa9
watkins <bendesign200@gmail.com>
2016-07-15 17:18:21 +0100

    Fix login error

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :topics
   resources :sessions
 
+  devise_scope :student do
+    match '/sessions/student', to: 'devise/sessions#create', via: :post
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
