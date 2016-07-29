37154b50927bb3fa0e8584016f2b410ed9f5c444
watkins <bendesign200@gmail.com>
2016-07-29 14:57:50 +0100

    Resolve #19: Deleting a topic doesn't delete sessions

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   belongs_to :subject
-  has_many :sessions
+  has_many :sessions, dependent: :destroy
   validates_uniqueness_of :name, scope: "subject_id"
 
   def name_with_subject_name
