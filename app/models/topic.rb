8354c68cc63335e53059f5be07031340c3ab7ba5
watkins <bendesign200@gmail.com>
2016-07-29 19:04:50 +0100

    Add length validation for name in topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,8 @@ class Topic < ApplicationRecord
   belongs_to :subject
   has_many :sessions, dependent: :destroy
   validates :name, presence: true,
-                   uniqueness: { scope: "subject_id" }
+                   uniqueness: { scope: "subject_id" },
+                   length: { in: 5..30 }
 
   def name_with_subject_name
     return "#{subject.name} - #{name}"
