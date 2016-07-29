9bd01b245f79f59a0505f7ec1d8a66bbad3c8714
watkins <bendesign200@gmail.com>
2016-07-29 14:54:20 +0100

    Resolve #8: Deleting a subject doesn't delete topics

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,6 @@
 class Subject < ApplicationRecord
   belongs_to :student
-  has_many :topics
+  has_many :topics, dependent: :destroy
   validates_presence_of :name, :teacher
   validates :name, length: { in: 5..30 }
   validates_uniqueness_of :name, scope: "student_id"
