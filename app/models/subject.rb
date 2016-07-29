a2b81320a7d6d03bc6bf941bbe0b589e2258a289
watkins <bendesign200@gmail.com>
2016-07-29 14:48:22 +0100

    Resolve #13: No length validation for subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
 class Subject < ApplicationRecord
   belongs_to :student
   has_many :topics
+  validates_presence_of :name, :teacher
+  validates :name, length: { in: 5..30 }
   validates_uniqueness_of :name, scope: "student_id"
 end
