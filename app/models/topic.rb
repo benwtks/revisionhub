62eb940bca982cab4e6ebfce5047c3b55e28f923
watkins <bendesign200@gmail.com>
2016-07-29 15:18:09 +0100

    Add validation to check presence of name for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :subject
   has_many :sessions, dependent: :destroy
+  validates_presence_of :name
   validates_uniqueness_of :name, scope: "subject_id"
 
   def name_with_subject_name
