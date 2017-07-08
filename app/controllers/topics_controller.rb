9acbd04192dad890972b0bae7cded7fe3a064f0b
watkins <bendesign200@gmail.com>
2017-07-08 20:42:13 +0100

    Added initial version of edit functionality for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,6 +3,19 @@ class TopicsController < ApplicationController
   before_action :find_topic, only: [:edit, :update, :destroy]
 
   def edit
+
+    # Note that this solution doesn't currently work.
+    # When the user tries to edit a topic while signed out they are presented with the error
+    # 'undefined method `subjects' for nil:NilClass' instead of being redirected to the sign up page
+    # When the user tries to edit a topic while signed into a different account to the one that the topic
+    # belongs to, they're presented with the error "Couldn't find Subject with 'id'=(topic id here) [WHERE `subjects`.`student_id` = ?]"
+    # instead of being redirected to the home page.
+
+    if !student_signed_in?
+      redirect_to new_student_session_path
+    elsif current_student != Student.find(@subject.student)
+      redirect_to root_path
+    end
   end
 
   def update
