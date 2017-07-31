fe77de27de80ef57bcce94c21bb8d6f9570e2662
watkins <bendesign200@gmail.com>
2017-07-31 20:29:12 +0100

    Alter sessions page layout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
   def session_subject_path(session)
-    subject_path(session.topic.subject)
+    subject_path(session.subject)
+  end
+
+  def session_date(session)
+    session.date.strftime("%e %B, %Y")
+  end
+
+  def session_link(session)
+    link_to session.name, session_subject_path(session)
   end
 end
