024d2fa59214ac08e6d3f5eaa0ec15a9b70ad848
watkins <bendesign200@gmail.com>
2017-07-31 22:13:16 +0100

    Minor refactoring

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,9 @@
 module TopicsHelper
+  def name_lowercase(subject)
+    subject.name.camelize(:lower)
+  end
+
+  def new_topic_heading(subject)
+    "New #{name_lowercase(subject)} topic"
+  end
 end
