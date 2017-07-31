dfa4a5422b3517116b3853e4c7b648eb55adfb2b
watkins <bendesign200@gmail.com>
2017-07-31 21:50:00 +0100

    Add count of sessions to subjects page

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,2 +1,18 @@
 module SubjectsHelper
+  def no_sessions(subject)
+    i = 0
+    subject.topics.each do |topic|
+      i += topic.sessions.count
+    end
+    
+    if i == 1
+      s = "once"
+    elsif i == 2
+      s = "twice"
+    else
+      s = "#{i} times"
+    end
+    
+    return "Revised #{s}"
+  end
 end
