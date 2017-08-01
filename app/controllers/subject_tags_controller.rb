e6574da34982036158c2e2c16ad146c50d376775
watkins <bendesign200@gmail.com>
2017-08-01 22:33:08 +0100

    Write methods for subject_tags_controller

--- app/controllers/subject_tags_controller.rb
+++ app/controllers/subject_tags_controller.rb
@@ -1,12 +1,25 @@
 class SubjectTagsController < ApplicationController
   def new
+    if student_signed_in?
+      @subject_tag = SubjectTag.new
+    else
+      redirect_to new_student_session_path, alert: "Please login to add subject tags"
   end
 
   def create
+    @subject_tag = SubjectTag.new(subject_tag_params)
+    @subject_tag[:student_id] = current_student.id
+
+    if @subject_tag.save
+      redirect_to root_path, notice: "Subject tag successfully created"
+    else
+      render 'new'
+    end
   end
 
   private
 
   def subject_tag_params
+    params.require(:subject_tag).permit(:name)
   end
 end
