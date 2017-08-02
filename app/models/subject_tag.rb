cd4fbf2c46034618c7aea8ed0eaaa56528bf8ba8
watkins <bendesign200@gmail.com>
2017-08-02 20:19:31 +0100

    Add tagColour association and name validation for subject_tag model

--- app/models/subject_tag.rb
+++ app/models/subject_tag.rb
@@ -1,4 +1,8 @@
 class SubjectTag < ApplicationRecord
   has_and_belongs_to_many :subjects
   belongs_to :student
+  has_one :tagColour
+
+  validates :name, presence: true,
+                   uniqueness: true
 end
