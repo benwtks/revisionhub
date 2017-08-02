7f6964b1c066d3b54d1262dc739e91457ada34cc
watkins <bendesign200@gmail.com>
2017-08-02 17:18:05 +0100

    Add associations

--- app/models/subject_tag.rb
+++ app/models/subject_tag.rb
@@ -1,3 +1,4 @@
 class SubjectTag < ApplicationRecord
   has_and_belongs_to_many :subjects
+  belongs_to :student
 end
--- app/models/topic_tag.rb
+++ app/models/topic_tag.rb
@@ -1,3 +1,4 @@
 class TopicTag < ApplicationRecord
   has_and_belongs_to_many :topics
+  belongs_to :student
 end
