41d97f803074a8c86b2f27be38b099716c4143c3
watkins <bendesign200@gmail.com>
2017-08-04 00:15:49 +0100

    Set up associations for tag colours

--- app/models/tag_colour.rb
+++ app/models/tag_colour.rb
@@ -1,2 +1,4 @@
 class TagColour < ApplicationRecord
+  has_many :subject_tags
+  has_many :topic_tags
 end
--- app/models/topic_tag.rb
+++ app/models/topic_tag.rb
@@ -1,7 +1,7 @@
 class TopicTag < ApplicationRecord
   has_and_belongs_to_many :topics
   belongs_to :student
-  has_one :tagColour
+  belongs_to :tagColour
 
   validates :name, presence: true,
                    uniqueness: true
