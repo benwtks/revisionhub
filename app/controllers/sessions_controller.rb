c0b11583e26ee11ba035425498cb4ef8a086d58d
watkins <bendesign200@gmail.com>
2017-08-09 22:33:55 +0100

    Minor changes including added redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,10 @@ class SessionsController < ApplicationController
   def index
     if student_signed_in?
       @sessions = current_student.sessions.order("date DESC, created_at DESC")
+
+      if @sessions.empty?
+        redirect_to new_session_path, alert: "Add sessions to view your revision timeline"
+      end
     else
       redirect_to new_student_session_path, alert: "Please login to view your revision log"
     end
@@ -9,8 +13,13 @@ class SessionsController < ApplicationController
 
   def new
     if student_signed_in?
-      @session = Session.new
       @topics = current_student.topics
+
+      if @topics.blank?
+        redirect_to root_path, alert: "You need to add topics to log revision sessions"
+      else
+        @session = Session.new
+      end
     else
       redirect_to new_student_session_path, alert: "Please login to log a revision session"
     end
