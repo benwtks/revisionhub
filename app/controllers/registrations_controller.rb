8863d84c126fdb94f7232041632943deca74c2cc
watkins <bendesign200@gmail.com>
2017-08-21 18:43:30 +0100

    Add after sign in path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    new_subject_path
+  end
 
   private
 
