1ca6fa750155935eb44ffeba875a122083be8520
watkins <bendesign200@gmail.com>
2017-08-21 22:23:40 +0100

    Design tweaks

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -2,4 +2,24 @@ module SubjectsHelper
   def no_sessions(subject)
     return "Revised #{nat_lang(subject.session_count)}"
   end
+
+  def time_of_day
+    current_time = Time.now.to_i
+    midnight = Time.now.beginning_of_day.to_i
+    noon = Time.now.middle_of_day.to_i
+    five_pm = Time.now.change(:hour => 17 ).to_i
+    eight_pm = Time.now.change(:hour => 20 ).to_i
+
+    if midnight.upto(noon).include?(current_time)
+      return "morning"
+    elsif noon.upto(five_pm).include?(current_time)
+      return "afternoon"
+    elsif eight_pm.upto(midnight + 1.day).include?(current_time)
+      return "evening"
+    end
+  end
+
+  def greeting(name)
+    return "Good #{time_of_day}, #{name}"
+  end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,6 @@
 class Session < ApplicationRecord
   belongs_to :topic
   belongs_to :student
+
+  default_scope -> { order("date DESC, created_at DESC") }
 end
