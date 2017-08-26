95679b2776901ad5ffe2eaae9b636267095d72f6
watkins <bendesign200@gmail.com>
2017-08-27 00:48:49 +0100

    Accept usernames for sign ins

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,10 +10,36 @@ class Student < ApplicationRecord
   has_many :subjectTags
   has_many :topicTags
 
-  validates :email, length: { in: 5..200 }
+  validates :email, length: { in: 5..200 },
+                    uniqueness: {
+                      :case_sensitive => false
+                    }
+
   validates :username, uniqueness: true
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
 
   def name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+end
 end
