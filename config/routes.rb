dd1f51bc9d09d8e4e4645d4e244b6594e97a7855
watkins <bendesign200@gmail.com>
2017-08-27 17:26:01 +0100

    Change devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :students, controllers: { registrations: 'registrations' }
+  devise_for :students, controllers: { registrations: 'registrations' }, skip: [:sessions, :registrations]
 
   get '/log', to: 'sessions#index'
   resources :sessions
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
 
   devise_scope :student do
     match '/sessions/student', to: 'devise/sessions#create', via: :post
+    get 'signin', to: 'devise/sessions#new', as: :new_student_session
+    post 'signin', to: 'devise/sessions#create', as: :student_session
+    get 'signup', to: 'devise/registrations#new', as: :new_student_registration
+    post '/signup', to: 'devise/registrations#create', as: :student_registration
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_student_session
   end
 
   authenticated :student do
