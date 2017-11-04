7f3c0e1d85f076f5fd74e3ade47e718fa630dbef
Watkins <bendesign200@gmail.com>
2017-11-04 17:42:07 +0000

    Add attr accessors for hours and minutes
    This will allow me to have the user set the hours and minutes for the duration of their revision sessions separately and then turn it into minutes in the controller to store in the db

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,5 +2,8 @@ class Session < ApplicationRecord
   belongs_to :topic
   belongs_to :student
 
+  attr_accessor :hours
+  attr_accessor :minutes
+
   default_scope -> { order("date DESC, created_at DESC") }
 end
