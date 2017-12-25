eaf265a8d4705d92fb7a8d530b6bf165b7e19c39
Watkins <bendesign200@gmail.com>
2017-12-25 18:15:58 +0000

    Add links for planned pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def home
-    redirect_to new_student_session_path
+    #redirect_to new_student_session_path
+  end
+
+  def blog
+    redirect_to root_path, notice: "Coming soon!"
+  end
+
+  def pricing
+    redirect_to root_path, notice: "Plus memberships coming soon!"
   end
 end
