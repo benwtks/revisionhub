3d01339f717802f3f88cb53d764726791733ad0b
Watkins <bendesign200@gmail.com>
2017-12-25 21:28:54 +0000

    Refactor formatted_duration method to be more general

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,19 +17,19 @@ module ApplicationHelper
      raw Rails.application.assets.find_asset(path + '.svg').to_s
    end
 
-   def formatted_duration(session)
-    if session.duration_hours > 0
-      if session.duration_hours == 1
+   def formatted_duration(duration)
+    if duration[0] > 0
+      if duration[0] == 1
         hours = "1 hr, "
       else
-        hours = "#{session.duration_hours} hrs, "
+        hours = "#{duration[0]} hrs, "
       end
     end
 
-    if session.duration_minutes == 1
+    if duration[1] == 1
       minutes = "1 min"
     else
-      minutes = "#{session.duration_minutes} mins"
+      minutes = "#{duration[1]} mins"
     end
 
     return "#{hours}#{minutes}"
