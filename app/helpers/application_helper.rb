133ec69cb6f2e7c29d359be1a30c8c1a12774701
Watkins <bendesign200@gmail.com>
2017-12-25 19:48:41 +0000

    Move formatted_duration helper to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,22 @@ module ApplicationHelper
    def inline_svg(path)
      raw Rails.application.assets.find_asset(path + '.svg').to_s
    end
+
+   def formatted_duration(session)
+    if session.duration_hours > 0
+      if session.duration_hours == 1
+        hours = "1 hr, "
+      else
+        hours = "#{session.duration_hours} hrs, "
+      end
+    end
+
+    if session.duration_minutes == 1
+      minutes = "1 min"
+    else
+      minutes = "#{session.duration_minutes} mins"
+    end
+
+    return "#{hours}#{minutes}"
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,22 +10,4 @@ module SessionsHelper
   def session_link(session)
     link_to topic_name(session), session_subject_path(session)
   end
-
-  def formatted_duration(session)
-    if session.duration_hours > 0
-      if session.duration_hours == 1
-        hours = "1 hr, "
-      else
-        hours = "#{session.duration_hours} hrs, "
-      end
-    end
-
-    if session.duration_minutes == 1
-      minutes = "1 min"
-    else
-      minutes = "#{session.duration_minutes} mins"
-    end
-
-    return "#{hours}#{minutes}"
-  end
 end
