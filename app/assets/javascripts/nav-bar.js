d8d0fe531ffdece006800d5837d56060e752ba18
Watkins <bendesign200@gmail.com>
2017-12-25 19:29:09 +0000

    Refactor nav bar cloning

--- app/assets/javascripts/nav-bar.js
+++ app/assets/javascripts/nav-bar.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  var $clone = $(".nav-bar").before($(".nav-bar").clone().addClass("clone"));
-
   $(window).on("scroll", function() {
     var fromTop = $(window).scrollTop();
     $("body").toggleClass("down", (fromTop > 400));
