ef5cb7ba843c2b3cbc9d99c3c15491108fa1407f
Ben Watkins <bendesign200@gmail.com>
2019-06-24 14:32:35 +0100

    Try to fix redirects

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,7 +4,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
     if signed_in?(resource_name)
-      signed_in_root_path(resource)
+      root_path
     else
       new_student_session_path(resource_name)
     end
