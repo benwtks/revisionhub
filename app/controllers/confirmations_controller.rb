c175cff62da0ebe2d399d919c8b0d4febe32db21
Ben Watkins <bendesign200@gmail.com>
2019-06-24 14:38:14 +0100

    Try debugging stuff

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,8 +4,10 @@ class ConfirmationsController < Devise::ConfirmationsController
   # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
     if signed_in?(resource_name)
+      puts "case 1"
       root_path
     else
+      puts "case 2"
       new_student_session_path(resource_name)
     end
   end
